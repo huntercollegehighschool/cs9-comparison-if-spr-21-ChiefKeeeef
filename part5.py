0c8e858d1fc7d628a70097e8542361fc2210bcc7
Curious George Kim <103281067+ChiefKeeeef@users.noreply.github.com>
2022-05-20 13:33:53 +0000

    Every part

--- part5.py
+++ part5.py
@@ -23,14 +23,14 @@ not a month
 '''
 
 #start writing your code below
-month = input("Enter a month: ")
-while month not in ['January', 'February', 'March', 'April','May','June','July','August','September','October', 'November', 'December']:
-  print("Enter a valid part of the assignment.")
-  month = input("Enter a month: ")
+month= input("Enter a month: ")
 
-if month = ('January'or 'March' or 'May' or 'July' or 'August' or 'October' or 'December'):
-  print("31")
-elif month = 'February'
+if month == "February" :
   print("28 or 29")
+elif month in ("April", "June", "September", "November"):
+  print("30 days")
+elif month in ("January", "March", "May", "July", "August", "October", "December"):
+  print("31 days")
 else:
-print("30")
+  print("not a month")
+
